f43ca5f376d68d254f3637d9acf06e3a40fc98f0
Rahul Bera <bera2591@gmail.com>
2021-05-24 21:47:43 +0530

    Home.jsx: Redesign the UI - Reformat the code - Segregate sections in components - Load encode & decode seperately - Modify canvas visibility - Remove redundant imports

--- hiyoco/src/Components/Home.jsx
+++ hiyoco/src/Components/Home.jsx
@@ -1,94 +1,87 @@
-import { useState, useEffect, useRef} from "react";
+import { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Box from '@material-ui/core/Box';
 import Lock from "../../src/lock.jpg";
-import { loadImage } from "../Utils/steganography";
+import { decode, encode } from "../Utils/steganography";
+import EncodeGrid from "./EncodeGrid";
+import UploadButton from "./UploadButton";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: '0',
+        backgroundColor: theme.palette.background.paper,
+        padding: '0',
     },
-    image:{
-      backgroundImage:`url(${Lock})`,
+    image: {
+        backgroundImage: `url(${Lock})`,
     },
     heroButtons: {
-      marginTop: theme.spacing(4),
+        marginTop: theme.spacing(4),
     },
-    app:{
-      backgroundColor:'#1ca9c9',
-      overflow:"hidden"
+    app: {
+        backgroundColor: '#1ca9c9',
+        overflow: "hidden"
     },
-    heading:{
-        color:"black",
-        fontWeight:"normal",
-        fontSize:"2.5rem"
+    heading: {
+        color: "black",
+        fontWeight: "normal",
+        fontSize: "2.5rem"
+    },
+    canvas: {
+        display: "none"
     }
-  }));
+}));
 
-export default function Home()
-{
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [imageURL, setImageURL] = useState(null);
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+export default function Home() {
+    const [option, setOption] = useState('home');
     const classes = useStyles();
 
-    function submitHandler(e)
-    {
-        e.preventDefault();
-        console.log(name + " " + email + " ");
+    function handleClick(event) {
+
+        const { name } = event.currentTarget;
+        if (name === 'home') {
+            setOption('home');
+            // document.getElementById('encoded-image').style.display = 'none';
+        } else if (name === 'encode') {
+            setOption('encode');
+        } else if (name === 'decode') {
+            setOption('decode');
+        }
     }
 
     return (
         <div className={classes.app} height="100vh">
             <div className={classes.heroContent}>
-                <Container maxWidth="xl"  style={{ backgroundColor: '#1ca9c9', width:'100%', padding:'2rem' }}>
+                <Container maxWidth="xl" style={{ backgroundColor: '#1ca9c9', width: '100%', padding: '2rem' }}>
                     <Typography component="h1" variant="h2" align="center" color="textPrimary" className={classes.heading} gutterBottom>
-                    Hide Your Copyright
+                        Hide Your Copyright
                     </Typography>
                     <Typography variant="h5" align="center" color="textPrimary" paragraph>
-                    Protect your images using stegnography techniques!
+                        Protect your images using stegnography techniques!
                     </Typography>
                 </Container>
             </div>
-            <div className={classes.heroContent} style={{backgroundColor:"#1ca9c9"}}>
-                <img src={imageURL} alt="imageURL" id="image" style={{display:"none"}}/>
-                <Grid container spacing={2} justify="center">
-                    <Grid item sm={10} md={8} lg={3} style={{ height:"35vh", padding:"1rem", marginTop:"1.5rem"}}>
-                        <form noValidate autoComplete="off">
-                            <Box display="flex" flexDirection="column">
-                                <TextField id="name" label="Name" value={name} onChange={e => setName(e.target.value)} required style={{marginBottom:"0.35rem"}} />
-                                <TextField id="email" label="Email" value={email} onChange={e => setEmail(e.target.value)} required  style={{marginBottom:"0.75rem"}} />
-                                {/* <input className={classes.input} type="file" onChange={fileChangeHandler} accept="image/jpeg"  style={{marginBottom:"0.75rem"}} /> */}
-                                <Button variant="contained"color="primary" type="submit" onClick={loadImage}>Submit</Button>
-                            </Box>
-                        </form>         
-                    </Grid>
-                    <Grid item xs={10} sm={10} md={5} lg={4} style={{backgroundColor:"#202020",marginLeft:"1rem", height:"35vh"}}>
-                        <img id="input-image" alt="" />
-                    </Grid>
-                    <Grid item xs={10} sm={10} md={5} lg={4} style={{backgroundColor:"#202020", marginLeft:"1rem", height:"35vh"}}>
-                        {/* <canvas ref={resultRef} width="500" height="750"/> */}
-                    </Grid>
-                </Grid>   
+            <div className={classes.heroContent} style={{ backgroundColor: "#1ca9c9" }} align="center">
+                {option === 'home' && <Button style={{ margin: '1rem' }} name='encode' onClick={handleClick} variant="contained">Encode</Button>}
+                {option === 'home' && <Button style={{ margin: '1rem' }} name='decode' onClick={handleClick} variant="contained">Decode</Button>}
+                {option !== 'home' && <UploadButton />}
+                {option === 'encode' && <EncodeGrid />}
+                {option === 'encode' && <Button style={{ margin: '1rem' }} onClick={encode} variant="contained">Encode</Button>}
+                {option === 'decode' && <Button style={{ margin: '1rem' }} onClick={decode} variant="contained">Decode</Button>}
+                {option !== 'home' && <Button style={{ margin: '1rem' }} name='home' onClick={handleClick} variant="contained">Return</Button>}
             </div>
-            <div style={{height:'32vh', backgroundColor:'#1ca9c9'}}></div>
-            <canvas id="canvas"></canvas>
+            <div style={{ height: '32vh', backgroundColor: '#1ca9c9' }}></div>
+            <canvas id="canvas" className={classes.canvas}></canvas>
         </div>
     );
 }
