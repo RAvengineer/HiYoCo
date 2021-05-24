956858efbe8a2b592722c5b5c0df25f39222e133
Rahul Bera <bera2591@gmail.com>
2021-05-24 19:34:11 +0530

    EncodeGrid.jsx: Create a Grid to extract info for encoding the input image

---
+++ hiyoco/src/Components/EncodeGrid.jsx
@@ -0,0 +1,34 @@
+import { Grid, Box, TextField, Button } from "@material-ui/core";
+import { useState } from "react";
+
+export default function EncodeGrid() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    function submitHandler(e) {
+        e.preventDefault();
+        console.log(name + "\n" + email);
+    }
+
+    return (
+        <Grid container spacing={2} justify="center">
+            <Grid item sm={10} md={8} lg={3} style={{ height: "35vh", padding: "1rem", marginTop: "1.5rem" }}>
+                <form noValidate autoComplete="off">
+                    <Box display="flex" flexDirection="column">
+                        <TextField id="name" label="Name" value={name} onChange={e => setName(e.target.value)} required style={{ marginBottom: "0.35rem" }} />
+                        <TextField id="email" label="Email" value={email} onChange={e => setEmail(e.target.value)} required style={{ marginBottom: "0.75rem" }} />
+                        {/* <input className={classes.input} type="file" onChange={fileChangeHandler} accept="image/jpeg"  style={{marginBottom:"0.75rem"}} /> */}
+                        <Button variant="contained" color="primary" type="submit" onClick={submitHandler}>Submit</Button>
+                    </Box>
+                </form>
+            </Grid>
+            <Grid item xs={10} sm={10} md={5} lg={4} style={{ backgroundColor: "#202020", marginLeft: "1rem", height: "35vh" }}>
+                <img id="input-image" alt="" style={{ height: "33vh", margin: "auto" }}/>
+            </Grid>
+            <Grid item zeroMinWidth xs={10} sm={10} md={5} lg={4} style={{ backgroundColor: "#202020", marginLeft: "1rem", height: "35vh" }}>
+                <img id="encoded-image" alt=""></img>
+            </Grid>
+        </Grid>
+    );
+}
