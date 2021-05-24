5363362f29eada4a2f299cbb574f4e96d17dcc17
Rahul Bera <bera2591@gmail.com>
2021-05-24 19:32:42 +0530

    UploadButton.jsx: Create custom button A copy from https://github.com/FOSS-Cell-GECPKD/image-stego

---
+++ hiyoco/src/Components/UploadButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { loadImage } from '../Utils/steganography';
+
+export default function UploadButton() {
+    return (
+        <label htmlFor="upload-photo">
+            <input
+                style={{ display: 'none' }}
+                id="upload-photo"
+                name="upload-photo"
+                type="file"
+                onChange={loadImage}
+            />
+            <div>
+                <Button style={{ margin: '1rem' }} variant="contained" component="span">
+                    Upload Image
+        </Button>
+            </div>
+        </label>
+    );
+}
