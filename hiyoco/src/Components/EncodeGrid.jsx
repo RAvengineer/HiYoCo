b18bdf94a74583e3517793fb1fc2157961cbc8c6
Rahul Bera <bera2591@gmail.com>
2021-05-25 00:00:50 +0530

    EncodeGrid.jsx: Modify details inputBox UI & logic - Remove Submit button - Remove redundant imports

--- hiyoco/src/Components/EncodeGrid.jsx
+++ hiyoco/src/Components/EncodeGrid.jsx
@@ -1,5 +1,4 @@
-import { Grid, Box, TextField, Button, makeStyles } from "@material-ui/core";
-import { useState } from "react";
+import { Grid, Box, TextField, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     imageBox: {
@@ -15,23 +14,13 @@ export default function EncodeGrid() {
 
     const classes = useStyles();
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-
-    function submitHandler(e) {
-        e.preventDefault();
-        console.log(name + "\n" + email);
-    }
-
     return (
         <Grid container spacing={2} justify="center">
             <Grid item sm={10} md={8} lg={3} style={{ height: "35vh", padding: "1rem", marginTop: "1.5rem" }}>
                 <form noValidate autoComplete="off">
                     <Box display="flex" flexDirection="column">
-                        <TextField id="name" label="Name" value={name} onChange={e => setName(e.target.value)} required style={{ marginBottom: "0.35rem" }} />
-                        <TextField id="email" label="Email" value={email} onChange={e => setEmail(e.target.value)} required style={{ marginBottom: "0.75rem" }} />
-                        {/* <input className={classes.input} type="file" onChange={fileChangeHandler} accept="image/jpeg"  style={{marginBottom:"0.75rem"}} /> */}
-                        <Button variant="contained" color="primary" type="submit" onClick={submitHandler}>Submit</Button>
+                        <TextField type="text" id="author-name" label="Name" required style={{ marginBottom: "0.35rem" }} />
+                        <TextField type="email" id="author-email" label="Email" required style={{ marginBottom: "0.75rem" }} />
                     </Box>
                 </form>
             </Grid>
