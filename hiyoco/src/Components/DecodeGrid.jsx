24d76d021dc439198b3e3886d3d3f7a8ead14d9d
Rahul Bera <bera2591@gmail.com>
2021-05-25 01:29:13 +0530

    DecodeGrid.jsx: Create a Grid for decode image to preview the image Basically, just copied & modified EncodeGrid.jsx :stuck_out_tongue_winking_eye:

---
+++ hiyoco/src/Components/DecodeGrid.jsx
@@ -0,0 +1,24 @@
+import { Grid, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    imageBox: {
+        backgroundColor: "#202020",
+        marginLeft: "1rem",
+        height: "35vh",
+        display: "flex",
+        justifyContent: "center"
+    }
+}));
+
+export default function DecodeGrid() {
+
+    const classes = useStyles();
+
+    return (
+        <Grid container spacing={2} justify="center">
+            <Grid item xs={10} sm={10} md={5} lg={4} className={classes.imageBox}>
+                <img id="input-image" alt="" />
+            </Grid>
+        </Grid>
+    );
+}
