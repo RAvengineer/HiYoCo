d1f6d90a40af301842792769a2fe3f890fbacbf5
Rahul Bera <bera2591@gmail.com>
2021-06-01 19:40:19 +0530

    UploadButton.jsx: Add props - id: For id of the input tag - onChange: Function to be executed onChanged in input tag

--- hiyoco/src/Components/UploadButton.jsx
+++ hiyoco/src/Components/UploadButton.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import { loadImage } from '../Utils/steganography';
 
-export default function UploadButton() {
+export default function UploadButton(props) {
     return (
         <label htmlFor="upload-photo">
             <input
                 style={{ display: 'none' }}
-                id="upload-photo"
+                id={props.id}
                 name="upload-photo"
                 type="file"
-                onChange={loadImage}
+                onChange={props.onChange}
             />
             <div>
                 <Button style={{ margin: '1rem' }} variant="contained" component="span">Upload Image</Button>
