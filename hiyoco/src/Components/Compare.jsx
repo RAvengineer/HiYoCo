96de6e744c7f6f86107db6b291ae06f293326d9a
Rahul Bera <bera2591@gmail.com>
2021-06-02 08:53:15 +0530

    Update Compare.jsx - Modify Grid nesting pattern - Use generalized UploadButton - Use ReactCompareImage for image comparision widget - Modify css for imageBox margin - Add state variable 'compare' - Modify fileChangeHandler to support different id & add exception handling - Refactor code - Remove redundant imports

--- hiyoco/src/Components/Compare.jsx
+++ hiyoco/src/Components/Compare.jsx
@@ -1,22 +1,22 @@
-import { useRef, useEffect, useState} from "react";
+import { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import { Typography, Container, Grid, Button } from '@material-ui/core';
+import UploadButton from "./UploadButton";
+import ReactCompareImage from 'react-compare-image';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: '0',
+        backgroundColor: theme.palette.background.paper,
+        padding: '0',
     },
     imageBox: {
         backgroundColor: "#202020",
@@ -26,91 +26,104 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: "center"
     },
     heroButtons: {
-      marginTop: theme.spacing(4),
+        marginTop: theme.spacing(4),
     },
-    app:{
-      backgroundColor:'#1ca9c9',
-      overflow:"hidden"
+    app: {
+        backgroundColor: '#1ca9c9',
+        overflow: "hidden"
     },
-    heading:{
-        color:"black",
-        fontWeight:"normal",
-        fontSize:"2.5rem"
+    heading: {
+        color: "black",
+        fontWeight: "normal",
+        fontSize: "2.5rem"
     }
-  }));
+}));
 
-export default function Compare()
-{
-    const [file1, setFile1] = useState("");
-    const [file2, setFile2] = useState("");
+export default function Compare() {
     const [image1URL, setImage1URL] = useState("");
     const [image2URL, setImage2URL] = useState("");
+    const [compare, setCompare] = useState(false);
     const classes = useStyles();
-    const image1Ref = useRef(null); 
-    const image2Ref = useRef(null);
- 
-    useEffect(()=>{
-        if(image1Ref.current){
-            const canvas = image1Ref.current;
-            const context = canvas.getContext("2d");
-            if(file1)
-            {
-                context.drawImage(document.getElementById("image1"),0,0,250,250)
-            }
-        }
-        if(image2Ref.current){
-            const canvas = image2Ref.current;
-            const context = canvas.getContext("2d");
-            if(file2)
-            {
-                context.drawImage(document.getElementById("image2"),0,0,250,250)
-            }
-        }
-    })
 
     function fileChangeHandler(e) {
         let reader = new FileReader();
         let file = e.target.files[0];
         reader.onloadend = function () {
-            if(e.target.id==="file1") 
-            {
-                setFile1(file);
+            if (e.target.id === "image1-upload") {
                 setImage1URL(reader.result);
-            }
-            else{
-                setFile2(file);
+            } else if (e.target.id === "image2-upload") {
                 setImage2URL(reader.result);
-            } 
+            } else {
+                alert("Something went wrong while file upload!");
+            }
         };
-        reader.readAsDataURL(file);
+        try {
+            reader.readAsDataURL(file);
+        } catch (error) {
+            alert("No image selected!");
+        }
+    }
+
+    function onCompare() {
+        setCompare(true);
     }
 
     return (
         <div className={classes.app} height="100vh">
             <div className={classes.heroContent}>
-                <Container maxWidth="xl"  style={{ backgroundColor: '#1ca9c9', width:'100%', padding:'2rem' }}>
+                <Container maxWidth="xl" style={{ backgroundColor: '#1ca9c9', width: '100%', padding: '2rem' }}>
                     <Typography component="h1" variant="h2" align="center" color="textPrimary" className={classes.heading} gutterBottom>
-                    Image Diff
+                        Image Diff
                     </Typography>
                 </Container>
             </div>
-            <div className={classes.heroContent} style={{backgroundColor:"#1ca9c9"}}>
-                <Grid container spacing={2} justify="center">
-                    <Grid item xs={10} sm={10} md={5} lg={3} className={classes.imageBox}>
-                        <input type="file" accpet="image/jpeg" onChange={fileChangeHandler} id="file1"/>
-                        <img id="image1" src={image1URL} alt="image1" style={{display:"none"}}/>
-                        <canvas ref={image1Ref} width="500" height="750"/>
+            <div className={classes.heroContent} style={{ backgroundColor: "#1ca9c9" }}>
+                <Grid container spacing={2} justify="center" >
+                    <Grid item xs={10} sm={10} md={5} lg={3} style={{ margin: '0.5vh' }}>
+                        <Grid item >
+                            <UploadButton id="image1-upload" onChange={fileChangeHandler} />
+                        </Grid>
+                        <Grid item className={classes.imageBox}>
+                            <img id="image1" src={image1URL} alt="" />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={10} sm={10} md={5} lg={3} className={classes.imageBox}>
-                        <input type="file" accpet="image/jpeg" onChange={fileChangeHandler} id="file2"/>
-                        <img id="image2" src={image2URL} alt="image2" style={{display:"none"}}/>
-                        <canvas ref={image2Ref}  width="500" height="750"/>
+                    <Grid item xs={10} sm={10} md={5} lg={3} style={{ margin: '0.5vh' }}>
+                        <Grid item >
+                            <UploadButton id="image2-upload" onChange={fileChangeHandler} />
+                        </Grid>
+                        <Grid item className={classes.imageBox}>
+                            <img id="image2" src={image2URL} alt="" />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={10} sm={10} md={5} lg={3} className={classes.imageBox}>     
+                    <Grid item xs={10} sm={10} md={5} lg={3} style={{ margin: '0.5vh' }}>
+                        <Grid item >
+                            <Button
+                                style={{ margin: '1rem' }}
+                                variant="contained"
+                                component="span"
+                                color="secondary"
+                                onClick={onCompare}
+                            >Compare</Button>
+                        </Grid>
+                        <Grid item className={classes.imageBox}>
+                            {
+                                compare &&
+                                <ReactCompareImage
+                                    leftImage={image1URL}
+                                    rightImage={image2URL}
+                                    leftImageLabel="Image 1"
+                                    rightImageLabel="Image 2"
+                                    aspectRatio="wider"
+                                    rightImageCss={{ objectFit: 'contain', objectPosition: 'top' }}
+                                    leftImageCss={{ objectFit: 'contain', objectPosition: 'top' }}
+                                    hover
+                                />
+                            }
+                        </Grid>
                     </Grid>
-                </Grid>   
+                </Grid>
             </div>
-            <div style={{height:'42vh', backgroundColor:'#1ca9c9'}}></div>
+            <div style={{ height: '42vh', backgroundColor: '#1ca9c9' }}></div>
         </div>
     );
 }
